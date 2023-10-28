062b44cc598c2cedc4ae8f36fea65fa739d08da6
mirfa695 <bmirfa6@gmail.com>
2023-10-28 11:28:06 +0530

    updated

--- lib/screens/home/profile_screen.dart
+++ lib/screens/home/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/custom_widgets/avatar.dart';
 import 'package:movies_app/custom_widgets/bg_image.dart';
 import 'package:movies_app/custom_widgets/custom_button.dart';
 import 'package:movies_app/custom_widgets/custom_text.dart';
@@ -22,47 +21,6 @@ class Profile extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Avatar(
-                          size: Dimensions.customHeight(context, 24.6),
-                          img:
-                              'https://media.discordapp.net/attachments/1008571051392909393/1143952503549988974/allan_santamaria_Sensitive_Middle_man_seated_on_a_terrace_in_a__d9150e56-c0e9-416c-96d2-dd49f9839f5d.png?width=966&height=966'),
-                       CustomText(text: 'User1', size: Dimensions.height10(context))
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Avatar(
-                          size: Dimensions.customHeight(context, 24.6),
-                          img:
-                              'https://media.discordapp.net/attachments/1008571051392909393/1143958066140807298/cbs2023_82910_illustration_of_a_cow_with_a_mustache._childrens__2cdaffa0-f248-404a-ada2-435fbc3df52d.png?width=966&height=966'),
-                      CustomText(text: 'User2', size: Dimensions.height15(context))
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Avatar(
-                          size: Dimensions.customHeight(context, 24.6),
-                          img:
-                              'https://media.discordapp.net/attachments/1008571051392909393/1143950354929701005/leajay_25364_witch_in_the_forest_with_a_black_cat_broom_crystal_7102fc5c-45a5-4e4a-b283-259eaf6411a9.png?width=966&height=966'),
-                      CustomText(text: 'User3', size: Dimensions.height15(context))
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Avatar(
-                          size: Dimensions.customHeight(context, 24.6),
-                          img:
-                              'https://media.discordapp.net/attachments/1008571051392909393/1143946087502188658/darianz_Compose_an_image_that_encapsulates_the_mid-journey_esse_7d2b9bed-01e1-4e22-af22-b1b6eb93a25d.png?width=966&height=966'),
-                      CustomText(text: 'User4', size: Dimensions.height15(context))
-                    ],
-                  ),
-                ],
-              ),
                SizedBox(
                 height: Dimensions.customHeight(context, 24.6),
               ),
